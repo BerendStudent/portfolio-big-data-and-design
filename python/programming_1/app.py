c7f829bdd135a8964a283b0e449cc5b4007b1a5e
BerendStudent <144696095+BerendStudent@users.noreply.github.com>
2025-09-09 14:53:47 +0200

    finish opdracht 1

---
+++ python/programming_1/app.py
@@ -0,0 +1,23 @@
+
+oreo = {
+    'cal': 210,
+    'fat': 10.5,
+    'carbohydrates': 27,
+    'protein': 2
+}
+
+oreo_nutrition = {}
+
+for field in oreo:
+    oreo_nutrition[field] = oreo[field] / 2
+
+number_ate = int(input('How many cookies did you eat? '))
+
+for field in oreo_nutrition:
+    number = oreo_nutrition[field] * number_ate
+    print(field, "consumed: ", number)
+    if field == 'cal' and number > 750:
+        overeating = True
+
+if overeating:
+    print('Stop eating those damned delicious cookies!')
