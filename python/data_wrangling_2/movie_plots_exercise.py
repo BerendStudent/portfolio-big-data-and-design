bcb1865976b7470688e942d17c7ebb094799d114
BerendStudent <144696095+BerendStudent@users.noreply.github.com>
2025-09-19 13:08:08 +0200

    finish movie_plots

--- python/data_wrangling_2/movie_plots_exercise.py
+++ python/data_wrangling_2/movie_plots_exercise.py
@@ -21,9 +21,10 @@ print(df.info)
 def get_number_of_origins(df): 
     """
     Returns a sorted dictionary with the number of movies with each origin
+    
     Example code:
     for i in get_number_of_origins(df):
-        print(i, origins[i])
+        print(i)
     """
     origins = {}
     for index, row in df.iterrows():
@@ -36,6 +37,8 @@ def get_number_of_origins(df):
     
     return sorted_dict
 
+for i in get_number_of_origins(df):
+        print(i)
 
 #4. Subsetting: select only the rows with Bollywood movies
 print(df.loc[df['Origin/Ethnicity'] == 'Bollywood'])
@@ -57,27 +60,67 @@ print(new_df.columns)
 #8. Create a new column "kimono" that is True if the Plot contains the word "kimono"
 #and false if not (tip: find a suitable Pandas string method).
 #Tip: use Pandas .astype(int) to convert the resulting Boolean in 0 or 1.
-new_df["kimono"] = False
-for index, row in df.iterrows():
-        current_plot = row['Plot']
-        if "Kimono" in current_plot:
-            new_df[row]["kimono"] = True
+def add_word_present(expected_str, target_df = new_df, new_column_name = "", type = bool, literal = False):
+    """
+    Adds a new column, based on whether the plot contains the target string
+    
+    New column name defaults to expected_str if not explicitely set
 
-print(new_df)
+    Example code:
+    has_gun_df = add_word_present("gun", new_df, "has_gun", int, True)
+    """
+    if new_column_name == "": # if left empty
+        new_column_name = expected_str
+
+    if literal: #Adds word termination markers, to check for exclusively the full word
+        expected_str = f"\b{expected_str}\b"
+
+    target_df[new_column_name] = target_df["Plot"].str.contains(expected_str, na=False).astype(type) # Defaults to FALSE
+    return target_df
+
+kimono_df = add_word_present("kimono", new_df)
+print(kimono_df)
 
 #9. Using your new column, pd.groupby() and another Pandas function, count the number of movies
 #with "kimono" in the plot, for the different origins.
-for country, group in new_df.groupby('Origin'):
-    print(country)
+
+def compare_column(target_column, group_column, target_df = new_df):
+    """
+    Counts how many entries are True/False in target_column, in rows with specific group_column values
+    
+    Example code:
+    origins = count_true_false_by_origin("kimono", 'Origin', my_df)
+    """
+    groups = {} # One entry per group_column value
+    for country, group in target_df.groupby(group_column): # Pandas group is NOT my group
+        column_series = group[target_column]
+        groups[country] = int(column_series.sum())
+
+    sorted_groups = dict(sorted(groups.items(), key=lambda item: item[1], reverse=True))
+    return sorted_groups
+    
+
+origins = compare_column("kimono", 'Origin')
+print(origins)
+
 
 #10. Using your earlier code, create a function add_word_present() with one argument (word),
 #that adds a column df[word] with a 1 if the word occurs in the plot,
 #and 0 if not.
 #Extra challenge: make sure that it's not counted if it's inside another word.
+has_gun_df = add_word_present("gun", new_df, "has_gun", int, True)
+print(has_gun_df.loc[0])
 
 #11. Write another function compare_origins() with one argument (word), that:
 #1. adds a column to your data frame (simply call your earlier function)
 #2. prints the proportion of movies for different origins containing that word
+def compare_origins(word):
+    worded_df = add_word_present(word)
+    origins = compare_column(word, 'Origin', worded_df)
+    for country in origins:
+        print(f"{country}: {origins[country]}")
+
+compare_origins("gun")
 
 #12. We need one more tweak: to really compare different cultures,
 #we need to account for the fact that the total number of movies is not the same.
@@ -85,7 +128,55 @@ for country, group in new_df.groupby('Origin'):
 #Hint: note that df.groupby(["Origin"])[word].count() will get you the number of movies, grouped by origin.
 #Also sort the result so that the percentage is descending.
 #Finally, make it user-friendly: print the word and what the numbers mean
+def percentage_of_movies(word):
+    """
+    Prints the percentage of each country's movies containing the target word
+
+    Example code:
+    percentage_of_movies("gun")
+    """
+    percentages = {}
+    worded_df = add_word_present(word)
+    origins = compare_column(word, 'Origin', worded_df)
+    total_movie_numbers = worded_df.groupby(["Origin"])[word].count().to_dict()
+    for country in origins:
+        percentages[country] = origins[country] / total_movie_numbers[country]
     
+    percentages = dict(sorted(percentages.items(), key=lambda item: item[1], reverse=True))
+
+    print(f"Percentage of movies whose plot contain the word: {word}")
+    for country in percentages:
+        current_percent = round((percentages[country] * 100), 2)
+        print(f"{country}: {current_percent}%")
+
+percentage_of_movies("gun")
+
 #You're done! Try out your function and paste your most interesting result
 #as a comment
 
+"""
+American: 18.79%
+Japanese: 12.54%
+Australian: 12.15%
+Hong Kong: 11.5%
+Canadian: 10.79%
+Punjabi: 10.71%
+British: 10.3%
+Filipino: 9.38%
+Malaysian: 8.57%
+Bollywood: 7.88%
+Russian: 7.33%
+Chinese: 7.13%
+South_Korean: 5.36%
+Bengali: 5.23%
+Tamil: 4.96%
+Telugu: 4.81%
+Malayalam: 3.93%
+Bangladeshi: 3.45%
+Turkish: 2.86%
+Marathi: 2.13%
+Kannada: 2.03%
+Egyptian: 1.49%
+Assamese: 0.0%
+Maldivian: 0.0%
+"""
---
+++ team/workshop/workshop.md
@@ -0,0 +1,18 @@
+### Usecases Data Mining
+- Politie
+- Online Reviews (Steam)
+- Fraude
+- Spam-filter
+
+
+### WTF momenten Data Mining
+1. Wat is data mining
+2. Hoe werkt data mining
+3. Hoe analyseerd hij text op sentiment???
+4. Hoe haalt een algoritme text uit 'vieze' bronnen
+5. Hoe ziet zo'n samengestelde dataset er dan uit?
+
+
+### Bronnen
+
+https://medium.com/@data-overload/sentiment-analysis-uncovering-emotions-in-text-with-a-real-world-case-study-145b214122fa
