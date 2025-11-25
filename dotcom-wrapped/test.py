1d03deab3b8214e66bef12c61e87c6925bf2f9b5
BerendStudent <144696095+BerendStudent@users.noreply.github.com>
2025-11-25 15:55:58 +0100

    upload class functionality

--- dotcom-wrapped/test.py
+++ dotcom-wrapped/test.py
@@ -2,63 +2,89 @@ import pandas as pd
 
 df = pd.read_csv('dotcom-wrapped/data/wrapped_bank_account_1.csv', on_bad_lines="skip")
 
-print(df.loc[0])
-
-exemptions_list = ['spaarrekening']
-
-def checkExemptCategories(string : str):
+class MoneyChecker:
     '''
-    Checks if the transaction description should exempt the transaction from net income and expenses.
+    Wrapper class for calculations
 
-    Returns TRUE if the transaction is valid, FALSE if it is exempt
+    data argument accepts pandas dataframe
     '''
-    string = string.lower()
-    wordlist = string.split()
-    for word in wordlist:
-        if word in exemptions_list:
+
+    exemptionslist = ['spaarrekening']
+
+    def __init__(self, data):
+        self.data = data
+    
+    def checkBank(self, string : str):
+        string = string.lower()
+        wordlist = string.split()
+        for word in wordlist:
+            if word == 'spaarrekening':
+                print()
+                return True
+        return False
+
+    def checkExemptions(self, string : str):
+        '''
+        Checks if the transaction description should exempt the transaction from net income and expenses.
+
+        Returns TRUE if the transaction is valid, FALSE if it is exempt
+        '''
+        string = string.lower()
+        wordlist = string.split()
+        for word in wordlist:
+            if word in self.exemptionslist:
+                return False
+        return True
+    
+    def getProfits(self):
+        df = self.data
+        total_in = df.loc[
+            (df['flow'] == 'inflow') & (df['description'].apply(self.checkExemptions)),
+            'amount'
+        ].sum()
+
+
+        total_out = df.loc[
+            (df['flow'] == 'outflow') & (df['description'].apply(self.checkExemptions)),
+            'amount'
+        ].sum()
+        return total_in - total_out
+    
+    def getCashOnHand(self):
+        return
+    
+    def getNetBank(self, accountID : str):
+        total_bank_out = df.loc[
+            (df['flow'] == 'inflow') & (df['description'].apply(self.checkBank)),
+            'amount'
+        ].sum()
+
+        total_bank_in = df.loc[
+            (df['flow'] == 'outflow') & (df['description'].apply(self.checkBank)),
+            'amount'
+        ].sum()
+        return total_bank_in - total_bank_out
+
+    def getTransactionsByDescription(self, description : str):
+        
+        return 0
+    
+    def checkColumnByString(self, category : str, string : str, strict = False):
+        if strict:
+            for index, row in df.iterrows():
+                if row[category] == string:
+                    return True
             return False
-    return True
+        else:
+            string = string.lower()
+            wordlist = string.split()
+            for index, row in df.iterrows():
+                for word in wordlist:
+                    if word in row[category]:
+                        return True
+            return False
+    
 
-def checkBank(string : str):
-    string = string.lower()
-    wordlist = string.split()
-    for word in wordlist:
-        if word == 'spaarrekening':
-            print()
-            return True
-    return False
+Account = MoneyChecker(df)
 
-total_in = df.loc[
-    (df['flow'] == 'inflow') & (df['description'].apply(checkExemptCategories)),
-    'amount'
-].sum()
-
-
-total_out = df.loc[
-    (df['flow'] == 'outflow') & (df['description'].apply(checkExemptCategories)),
-    'amount'
-].sum()
-
-total_bank = 10000
-
-total_bank_out = df.loc[
-    (df['flow'] == 'inflow') & (df['description'].apply(checkBank)),
-    'amount'
-].sum()
-
-total_bank_in = df.loc[
-    (df['flow'] == 'outflow') & (df['description'].apply(checkBank)),
-    'amount'
-].sum()
-
-
-print(f"Incomes: {total_in}, outflow: {total_out}")
-
-total_sum = total_in - total_out
-
-print(f"Net change: {total_sum}")
-
-total_bank += (total_bank_in - total_bank_out)
-
-print(f"Money in your account: ${total_bank}")
-
+print(Account.getProfits())
